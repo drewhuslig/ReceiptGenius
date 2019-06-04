a7ee5f676914930ea599203658a6844b490bb951
Drew Huslig <drewhuslig@gmail.com>
2019-06-04 07:00:15 -0500

    moved a lot of functions to modal component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ export default class App extends React.Component {
     super();
     this.state = {
       total: 0,
-      startDate: new Date(),
       data: [],
-      selectedOption: "",
-      description: ''
+      
     }
   }
 
@@ -36,14 +34,7 @@ export default class App extends React.Component {
     });
     this.setState({total: parseFloat(t).toFixed(2)})
   }
-
-  //Handles date for date picker
-  handleChangeDate = date => {
-    this.setState({
-      startDate: date
-    })
-  }
-
+  
   //Removes receipt from DB and updates state
   removeItem = (id, e) => {
     e.preventDefault();
@@ -60,64 +51,16 @@ export default class App extends React.Component {
     const modal = this.refs.modal;
     modal.style.display = 'block';
   }
-
-  //Functions to handle form data in modal
-  handleChangeOption = e => {
-    this.setState({
-      selectedOption: e.target.value
-    })
-  }
-
-  handleChangeFile = e => {
-    this.setState({file: e.target.files[0]})
-  }
-
-  handleChangeDescription = e => {
-    this.setState({description: e.target.value})
-  }
-
-  handleChangeTotal = value => {
-    this.setState({total: value})
-  }
-
-  handleChangeName = e => {
-    this.setState({name: e.target.value})
-  }
   
   //Adds receipt to DB and updates State
-  addReceipt = e => {
+  addReceipt = (e, formData) => {
     this.closeModal(e);
-    let formData = new FormData()
-    formData.append('file', this.state.file);
-    formData.append('name', this.state.name);
-    formData.append('total', this.state.total);
-    formData.append('data', this.state.startDate.toLocaleDateString("en-US"))
-    formData.append('category', this.state.selectedOption);
-    formData.append('description', this.state.description);
     Axios.post('/receipts', formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
     }).then(res => {
       this.setState({data: res.data}, this.calculateTotal(res.data));
-    })
-  }
-
-  //Toggles radio button off if already clicked
-  handleLabelClick = (e, button) => {
-    if(button.checked){
-      e.preventDefault();
-      button.checked = false;
-      this.setState({selectedOption: ""})
-    }
-  }
-
-  //Used for number formater
-  handleTotal = (e) => {
-    this.setState({
-      total: e.target.value
-    }, function() {
-      e.target.value = e.target.value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
     })
   }
 
@@ -170,15 +113,7 @@ export default class App extends React.Component {
         <div className='modal' ref='modal' onClick={e => this.outsideClick(e)}>
           <Modal
             ref='modalComponent'
-            handleChangeDate = {this.handleChangeDate}
-            handleChangeOption = {this.handleChangeOption}
-            handleChangeFile = {this.handleChangeFile}
-            handleChangeDescription = {this.handleChangeDescription}
-            handleChangeTotal = {this.handleChangeTotal}
-            handleChangeName = {this.handleChangeName}
-            handleLabelClick = {this.handleLabelClick}
             addReceipt = {this.addReceipt}
-            startDate = {this.state.startDate}
             closeModal = {this.closeModal}
           />
         </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,7 +7,10 @@ export default class Modal extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      message: ''
+      message: '',
+      selectedOption: "",
+      description: '',
+      startDate: new Date()
     }
     
   }
@@ -28,9 +31,65 @@ export default class Modal extends React.Component {
     } else {
       this.setState({message: ""});
       this.refs.form.reset();
-      this.props.addReceipt(e);
+      let formData = new FormData()
+      formData.append('file', this.state.file);
+      formData.append('name', this.state.name);
+      formData.append('total', this.state.total);
+      formData.append('data', this.state.startDate.toLocaleDateString("en-US"))
+      formData.append('category', this.state.selectedOption);
+      formData.append('description', this.state.description);
+      this.props.addReceipt(e, formData);
     } 
   }
+
+  //Functions to handle form data in modal
+  handleChangeOption = e => {
+    this.setState({
+      selectedOption: e.target.value
+    })
+  }
+
+  handleChangeFile = e => {
+    this.setState({file: e.target.files[0]})
+  }
+
+  handleChangeDescription = e => {
+    this.setState({description: e.target.value})
+  }
+
+  handleChangeTotal = value => {
+    this.setState({total: value})
+  }
+
+  handleChangeName = e => {
+    this.setState({name: e.target.value})
+  }
+
+  //Toggles radio button off if already clicked
+  handleLabelClick = (e, button) => {
+    if(button.checked){
+      e.preventDefault();
+      button.checked = false;
+      this.setState({selectedOption: ""})
+    }
+  }
+
+  //Used for number formater
+  handleTotal = (e) => {
+    this.setState({
+      total: e.target.value
+    }, function() {
+      e.target.value = e.target.value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    })
+  }
+
+  //Handles date for date picker
+  handleChangeDate = date => {
+    this.setState({
+      startDate: date
+    })
+  }
+
 
     render(){
         return(
@@ -45,20 +104,20 @@ export default class Modal extends React.Component {
                 <form ref='form'>
                   <div>
                     Receipt file*<br/>
-                    <input type='file' name='receiptUpload' ref='fileSelect' className='fileSelect' onChange={e => {this.props.handleChangeFile(e)}} />
+                    <input type='file' name='receiptUpload' ref='fileSelect' className='fileSelect' onChange={e => {this.handleChangeFile(e)}} />
                   </div>
                   <br/>
                   <div>
                     Vendor/Retailer*<br/>
-                    <input type='text' name='name' ref='name' onChange={e => this.props.handleChangeName(e)}></input>
+                    <input type='text' name='name' ref='name' onChange={e => this.handleChangeName(e)}></input>
                   </div>
                   <br/>
                   <div>
                     Transaction Date*<br/>
                     <DatePicker
                     dateFormat='yyy/MM/dd'
-                    selected={this.props.startDate} 
-                    onChange={this.props.handleChangeDate} /><span className='far fa-calendar'></span>
+                    selected={this.state.startDate} 
+                    onChange={this.handleChangeDate} /><span className='far fa-calendar'></span>
                   </div>
                   <br/>
                   <div>
@@ -67,25 +126,25 @@ export default class Modal extends React.Component {
                       const {formattedValue, value} = values;
                       // formattedValue = $2,223
                       // value ie, 2223
-                      this.props.handleChangeTotal(value)
+                      this.handleChangeTotal(value)
                     }}/>
                   </div>
                   <br/>
                   <div className='radio-group'>Category<br/>
                     <input type="radio" className="radio-button" name="category" value='Supplies' id='button1' ref='button1'
-                      onChange={this.props.handleChangeOption}/>
-                    <label htmlFor="button1" className='radio-label' onClick={e => this.props.handleLabelClick(e, this.refs.button1)}>Supplies</label>
+                      onChange={this.handleChangeOption}/>
+                    <label htmlFor="button1" className='radio-label' onClick={e => this.handleLabelClick(e, this.refs.button1)}>Supplies</label>
                     <input type="radio" className="radio-button" name="category" value='Subscriptions' id='button2' ref='button2'
-                      onChange={this.props.handleChangeOption}/>
-                    <label htmlFor="button2" className='radio-label' onClick={e => this.props.handleLabelClick(e, this.refs.button2)}>Subscriptions</label>
+                      onChange={this.handleChangeOption}/>
+                    <label htmlFor="button2" className='radio-label' onClick={e => this.handleLabelClick(e, this.refs.button2)}>Subscriptions</label>
                     <input type="radio" className="radio-button" name="category" value='Personal' id='button3' ref='button3'
-                      onChange={this.props.handleChangeOption}/>
-                    <label htmlFor="button3" className='radio-label' onClick={e => this.props.handleLabelClick(e, this.refs.button3)}>Personal</label>
+                      onChange={this.handleChangeOption}/>
+                    <label htmlFor="button3" className='radio-label' onClick={e => this.handleLabelClick(e, this.refs.button3)}>Personal</label>
                   </div>
                   <br/>
                   <div>
                     Description<br></br>
-                    <input type='text' name='description' onChange={e => this.props.handleChangeDescription(e)}></input><br/>
+                    <input type='text' name='description' onChange={e => this.handleChangeDescription(e)}></input><br/>
                   </div>
                   
                   * Indicates a required field<br/>
